852529bf33d290ed4b157ec82e0bc7e75402f583
Artyom Peshkov <artemyipeshkov@gmail.com>
2025-02-23 14:21:49 +0300

    fix

--- src/color.cpp
+++ src/color.cpp
@@ -1,13 +1,14 @@
 #include "color.hpp"
 
 #include <cstdint>
+#include <cmath>
 
 Color::Color(double red, double green, double blue): red(red), green(green), blue(blue) {};
 
 std::ostream& operator<<(std::ostream& os, const Color& color) {
-    uint8_t r = static_cast<uint8_t>(color.red * 255);
-    uint8_t g = static_cast<uint8_t>(color.green * 255);
-    uint8_t b = static_cast<uint8_t>(color.blue * 255);
+    uint8_t r = static_cast<uint8_t>(std::round(color.red * 255.0));
+    uint8_t g = static_cast<uint8_t>(std::round(color.green * 255.0));
+    uint8_t b = static_cast<uint8_t>(std::round(color.blue * 255.0));
 
     os << r << g << b;
     return os;
